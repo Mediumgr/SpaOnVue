fda14e896f691730ec4556863ce3b3c6d257cd33
Руслан <Mediumgr@yandex.ru>
2021-07-03 02:25:08 +0300

    test spaonvue

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+<div id="app">
+  <v-app id="inspire">
+    <v-app id="inspire">
+      <v-navigation-drawer
+        v-model="drawer" temporary
+        app
+      > <!-- temporary - затемнение фона при клике -->
+        <v-list dense>
+          <v-list-item link v-for="link of links" :key="link.title" :to="link.url">
+            <v-list-item-action>
+              <v-icon>{{link.icon}}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>{{link.title}}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-app-bar
+        app
+        color="cyan lighten-1"
+        dark
+      >
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="d-md-none d-lg-flex"></v-app-bar-nav-icon>
+        <v-toolbar-title>Application</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-toolbar-title class="d-none d-md-flex">
+          <v-btn color="cyan lighten-1" v-for="link of links" :key="link.icon" :to="link.url" :tile=true>
+            <span class="material-icons">{{link.icon}}</span>
+            <pre> </pre>{{link.title}}
+        </v-btn>
+        </v-toolbar-title>
+      </v-app-bar>
+      <v-main>
+        <router-view></router-view>
+      </v-main>
+      <v-footer
+        color="indigo"
+        app
+      >
+        <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
+      </v-footer>
+    </v-app>
+  </v-app>
+</div>
+</template>
+
+<script>
+
+export default {
+/*   name: 'App',
+
+  data: () => ({
+    //
+  }) */
+  data () {
+    return {
+      drawer: false,
+      source: '',
+      links: [
+        { title: 'Login', icon: 'lock', url: '/login' },
+        { title: 'Registration', icon: 'face', url: '/registration' },
+        { title: 'Orders', icon: 'bookmark_border', url: '/orders' },
+        { title: 'New ad', icon: 'note_add', url: '/new' },
+        { title: 'My ads', icon: 'list', url: '/list' }
+      ]
+    }
+  }
+}
+</script>
---
+++ src/components/Auth/Login.vue
@@ -0,0 +1,69 @@
+<template>
+<v-container
+          class="fill-height"
+          fluid
+        >
+          <v-row
+            align="center"
+            justify="center"
+          >
+            <v-col
+              cols="12"
+              sm="8"
+              md="4"
+            >
+              <v-card class="elevation-12">
+                <v-toolbar
+                  color="cyan lighten-1"
+                  dark
+                  flat
+                >
+                  <v-toolbar-title>Login form</v-toolbar-title>
+                </v-toolbar>
+                <v-card-text>
+                  <v-form v-model="valid" ref="form" validation>
+                    <v-text-field
+                      label="Email"
+                      name="email"
+                      prepend-icon="mdi-account"
+                      type="email"
+                      v-model="email"
+                    ></v-text-field>
+                    <v-text-field
+                      id="password"
+                      label="Password"
+                      name="password"
+                      prepend-icon="mdi-lock"
+                      type="password"
+                      v-model="password"
+                    ></v-text-field>
+                  </v-form>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="cyan lighten-1" @click="onSubmit">Login</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      email: '',
+      password: '',
+      valid: false
+    }
+  },
+  methods: {
+    onSubmit () {
+
+    }
+  }
+}
+// начать с 6 видео 8:43
+</script>
